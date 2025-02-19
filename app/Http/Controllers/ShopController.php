f01cdb0b299bba0e5352cffe10b03c501adca1fc
Khaled Kamr <200013171@must.edu.eg>
2025-02-19 14:55:00 +0200

    Update cart's items quantity

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Surfsidemedia\Shoppingcart\Facades\Cart;
 
 class ShopController extends Controller
 {
@@ -16,5 +17,19 @@ class ShopController extends Controller
         $product = Product::where('slug', $product_slug)->first();
         $related_products = Product::where('slug', '<>', $product_slug)->get()->take(8);
         return view("details", compact("product", "related_products"));
+    }
+
+    public function increase_cart_quantity($rowId) {
+        $product = Cart::instance('cart')->get($rowId);
+        $qty = $product->qty + 1;
+        Cart::instance('cart')->update($rowId, $qty);
+        return redirect()->back();
+    }
+
+    public function decrease_cart_quantity($rowId) {
+        $product = Cart::instance('cart')->get($rowId);
+        $qty = $product->qty - 1;
+        Cart::instance('cart')->update($rowId, $qty);
+        return redirect()->back();
     }
 }
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -66,8 +66,16 @@
                             <td>
                                 <div class="qty-control position-relative">
                                     <input type="number" name="quantity" value="{{$item->qty}}" min="1" class="qty-control__number text-center">
-                                    <div class="qty-control__reduce">-</div>
-                                    <div class="qty-control__increase">+</div>
+                                    <form action="{{route('cart.qty.decrease', $item->rowId)}}" method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                        <div class="qty-control__reduce">-</div>
+                                    </form>
+                                    <form action="{{route('cart.qty.increase', $item->rowId)}}" method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                        <div class="qty-control__increase">+</div>
+                                    </form>
                                 </div>
                             </td>
                             <td>
@@ -138,3 +146,17 @@
     </section>
   </main>
 @endsection
+
+@push('scripts')
+<script>
+    $(function() {
+        $(".qty-control__increase").on("click", function() {
+            $(this).closest('form').submit();
+        });
+
+        $(".qty-control__reduce").on("click", function() {
+            $(this).closest('form').submit();
+        });
+    })
+</script>
+@endpush
